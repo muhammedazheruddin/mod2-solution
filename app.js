d2b2338ae109bd51828324cfd280780958d6f2ce
Azheruddin <muhammedazheruddin@gmail.com>
2016-09-26 09:58:09 +0530

    Module 2 Solution

--- app.js
+++ app.js
@@ -1,49 +1,146 @@
 (function () {
 'use strict';
 
-var shoppingList2 = [
-  {
-    name: "Milk",
-    quantity: "2"
-  },
-  {
-    name: "Donuts",
-    quantity: "200"
-  },
-  {
-    name: "Cookies",
-    quantity: "300"
-  },
-  {
-    name: "Chocolate",
-    quantity: "5"
-  },
-  {
-    name: "Kiwis",
-    quantity: "25"
-  },
-  {
-    name: "Pappya",
-    quantity: "15"
-  }
-  
-];
+angular.module('ShoppingListCheckOff', [])
+.controller('ShoppingController', ShoppingController)
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController',AlreadyBoughtShoppingController)
+.service("ShoppingListCheckOffService", ShoppingListCheckOffService)
+//.config(Config);
+// .provider('ShoppingListCheckOffService', ShoppingListCheckOffServiceProvider)
 
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+// Config.$inject = ['ShoppingListServiceProvider'];
+// function Config() {
+// }
 
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList2 = shoppingList2;
 
-  $scope.addToList = function () {
-    var newItem = {
-      name: $scope.newItemName,
-      quantity: $scope.newItemQuantity
-    };
-
-    $scope.shoppingList2.push(newItem);
+ShoppingController.$inject = ['$scope','ShoppingListCheckOffService'];
+function ShoppingController($scope, ShoppingListCheckOffService) {
+  var list = this;
+  const noBuyMsg = 'Everything is bought!';
+  const noBoughtMsg = 'Nothing bought yet';
+  list.boughtMessage = noBoughtMsg;
+  $scope.showMsg = function () {
+    if (ShoppingListCheckOffService.getBoughtItems().length == 0) {
+      list.boughtMessage = noBoughtMsg;
+    } else {
+      list.boughtMessage = '';
+    }
+    if (ShoppingListCheckOffService.getBuyItems().length == 0) {
+      list.toBuyMessage= noBuyMsg;
+    } else {
+      list.toBuyMessage= ' ';
+    }
   };
 }
+
+
+
+ToBuyShoppingController.$inject = ['$scope','ShoppingListCheckOffService'];
+function ToBuyShoppingController($scope, ShoppingListCheckOffService) {
+  var list = this;
+  list.items = ShoppingListCheckOffService.getDefaultToBuyItems();
+  list.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeBuyItem(itemIndex);
+    $scope.showMsg();
+  };
+}
+
+AlreadyBoughtShoppingController.$inject = ['$scope','ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController($scope, ShoppingListCheckOffService) {
+  var list = this;
+  list.items = ShoppingListCheckOffService.getBoughtItems();
+  list.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeBoughtItem(itemIndex);
+    $scope.showMsg();
+  };
+}
+
+
+
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var boughtItems = [];
+
+  var defaultToBuyItems = [
+                            { name: "Kiwis", quantity: 30 },
+                            { name: "Pappaya", quantity: 20 },
+                            { name: "Promogranate", quantity: 10 },
+                            { name: "Grapes", quantity: 15 },
+                            { name: "Oranges", quantity: 50 }                          ];
+
+  var toBuyItems = defaultToBuyItems;
+
+  service.addBuyItem = function (itemName, quantity) {
+    // if ((maxItems === undefined) ||
+    //     (maxItems !== undefined) && (items.length < maxItems)) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      toBuyItems.push(item);
+    // }
+    // else {
+    //   throw new Error("Max items (" + maxItems + ") reached.");
+    // }
+  };
+
+  service.removeBuyItem = function (itemIndex) {
+    boughtItems.push(toBuyItems[itemIndex]);
+    toBuyItems.splice(itemIndex, 1);
+  };
+
+  service.getBuyItems = function () {
+    return toBuyItems;
+  };
+  service.getDefaultToBuyItems = function () {
+    return defaultToBuyItems;
+  };
+
+
+
+  service.addBoughtItem = function (itemName, quantity) {
+    // if ((maxItems === undefined) ||
+    //     (maxItems !== undefined) && (items.length < maxItems)) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      boughtItems.push(item);
+    // }
+    // else {
+    //   throw new Error("Max items (" + maxItems + ") reached.");
+    // }
+  };
+
+  service.removeBoughtItem = function (itemIndex) {
+    toBuyItems.push(boughtItems[itemIndex]);
+    boughtItems.splice(itemIndex, 1);
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+
+}
+
+
+// function ShoppingListCheckOffServiceProvider() {
+//   var provider = this;
+//
+//   provider.defaults = {
+//     maxItems: 10
+//   };
+//
+//   provider.$get = function () {
+//     var shoppingList = new ShoppingListCheckOffService(provider.defaults.maxItems);
+//
+//     return shoppingList;
+//   };
+// }
 
 })();
